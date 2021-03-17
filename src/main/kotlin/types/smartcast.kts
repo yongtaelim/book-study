da565376a62c09f3465c028508679f57119f4e4a
임용태 <lyt032410@gmail.com>
2021-03-17 14:40:00 +0900

    study kotlin types

---
+++ src/main/kotlin/types/smartcast.kts
@@ -0,0 +1,19 @@
+// 여기에 스마트 캐스트를 적용해보자.
+fun WhatToDo(dayOfWeek: Any) = when (dayOfWeek) {
+    "Saturday", "Sunday" -> "Relax"
+    in listOf("Monday", "TuesDay", "Wednesday", "Thursday") -> "Work hard"
+    in 2..4 -> "Work hard"
+    "Friday" -> "Party"
+    is String -> "What???"
+    else -> "No clue"
+}
+
+// Apply
+fun WhatToDo(dayOfWeek: Any) = when (dayOfWeek) {
+    "Saturday", "Sunday" -> "Relax"
+    in listOf("Monday", "TuesDay", "Wednesday", "Thursday") -> "Work hard"
+    in 2..4 -> "Work hard"
+    "Friday" -> "Party"
+    is String -> "What, .... length ${dayOfWeek.length}"
+    else -> "No clue"
+}
