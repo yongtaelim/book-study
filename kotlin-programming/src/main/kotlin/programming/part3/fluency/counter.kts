b6cf9e56acfcc8728c2b7634eb2a9eb6b7237bf9
limyongtae <lyt032410@gmail.com>
2021-05-27 23:19:08 +0900

    study

--- kotlin-programming/src/main/kotlin/programming/part3/fluency/counter.kts
+++ kotlin-programming/src/main/kotlin/programming/part3/fluency/counter.kts
@@ -1,3 +1,5 @@
+import java.math.BigInteger
+
 class Counter(val value: Int) {
     operator fun inc() = Counter(value + 1)
     operator fun dec() = Counter(value - 1)
@@ -10,3 +12,8 @@ class Counter(val value: Int) {
 오버로딩 연산자는 일반적인 연산자의 동작이어야한다
 변수이름을 의미있게 만들어라. 그래야 오버로딩의 문맥을 파악하기 좋다.
  */
+
+val one = BigInteger("1")
+val two = BigInteger("2")
+
+val result = one + two
