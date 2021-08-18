9238897886372339735d0929f139e20ffe6666f4
limyongtae <lyt032410@gmail.com>
2021-08-18 22:55:26 +0900

    study object

---
+++ object/src/main/kotlin/com/jessyt/object/chapter2/sub4/Document.kt
@@ -0,0 +1,29 @@
+package com.jessyt.`object`.chapter2.sub4
+
+import java.io.InputStream
+
+/**
+ * Created by LYT to 2021/08/18
+ */
+//class Document {
+//    fun write(content: InputStream): Int {
+//        // file 내용 저장
+//        // 저장된 바이트 수를 반환
+//        return 0
+//    }
+//}
+
+class Document {
+    fun output(): OutputPipe {}
+}
+
+class OutputPipe {
+    // file 저장
+    fun write(content: InputStream) {}
+
+    // 저장된 파일의 byte 반환
+//    fun bytes(): Int {}
+
+    // 저장 트랜잭션 시간 반환
+//    fun time(): Long {}
+}
