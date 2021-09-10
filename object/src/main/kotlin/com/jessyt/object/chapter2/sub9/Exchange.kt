42755bf34e014404b19fc48aaca462a50a6a91e5
limyongtae <lyt032410@gmail.com>
2021-09-10 19:38:48 +0900

    object study

--- object/src/main/kotlin/com/jessyt/object/chapter2/sub9/Exchange.kt
+++ object/src/main/kotlin/com/jessyt/object/chapter2/sub9/Exchange.kt
@@ -8,11 +8,34 @@ interface BadExchange {
     fun rate(source: String, target: String): Float
 }
 
-interface Exchange {
+interface GoodExchange {
     fun rate(source: String, target: String): Float
     class Smart {
-        private lateinit var origin: Exchange
+        private lateinit var origin: GoodExchange
         fun toUsd(source: String) =
             this.origin.rate(source, "USD")
+
+        fun eurToUsd() = this.toUsd("EUR")
+    }
+}
+
+class Test {
+    fun use() {
+        val rate1 = GoodExchange.Smart().toUsd("EUR")
+
+        val rate2 = GoodExchange.Smart().eurToUsd()
+    }
+}
+
+interface Exchange {
+    fun rate(origin: String, target: String): Float
+    class Fast: Exchange {
+        private lateinit var origin: Exchange
+        override fun rate(source: String, target: String): Float {
+            return if (source == target) 1.0f
+            else origin.rate(source, target)
+        }
+
+        fun toUsd(source: String) = this.origin.rate(source, "USD")
     }
 }
