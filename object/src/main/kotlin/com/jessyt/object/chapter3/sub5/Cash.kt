9827ed55caa78f35473c47c695320b08d4a1b8fe
limyongtae <lyt032410@gmail.com>
2021-09-24 09:52:52 +0900

    object study

---
+++ object/src/main/kotlin/com/jessyt/object/chapter3/sub5/Cash.kt
@@ -0,0 +1,13 @@
+package com.jessyt.`object`.chapter3.sub5
+
+/**
+ * Created by LYT to 2021/09/24
+ */
+class Cash(
+    private varn dollars: Int
+) {
+    fun getDollars() = this.dollars
+    fun setDollars(dollars: Int) {
+        this.dollars = dollars
+    }
+}
