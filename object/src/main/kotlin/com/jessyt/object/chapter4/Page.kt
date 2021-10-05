c10fb2a93f6f44ddaa732824c310e6839e176fb9
limyongtae <lyt032410@gmail.com>
2021-10-05 09:58:59 +0900

    study object

---
+++ object/src/main/kotlin/com/jessyt/object/chapter4/Page.kt
@@ -0,0 +1,33 @@
+package com.jessyt.`object`.chapter4
+
+/**
+ * Created by LYT to 2021/10/02
+ */
+class Page(
+    val title: String?
+) {
+    fun badTitle(): String? {
+        if (title == null) {
+            return null
+        }
+        return "Elegant Objects"
+    }
+
+    fun title(): String {
+        if (title == null) {
+            throw IllegalArgumentException("타이틀이 존재하지 않습니다.")
+        }
+        return "Elegant Objects"
+    }
+}
+
+fun use() {
+    val page = Page(null)
+    val title = page.title
+    if (title == null) {
+        println("Can't print. it's not a tile.")
+        return
+    }
+    println("title length=${title.length}")
+}
+
